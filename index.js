62167986bf1276a2682895416ee727f5db7cf0be
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-09-28 09:04:57 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const writeCards = (arr, event) => {
+    const messages = [];
+    
+    arr.forEach(name => messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`));
+
+    return messages;
+};
+
+const countDown = (nb) => {
+    for (let i = nb; i >= 0; i--)
+        console.log(i);
+};
